92c5885950773904395fd36e2d23fcaf2431f3c5
sachin810 <35040906+sachin810@users.noreply.github.com>
2018-04-01 19:38:40 +0530

    added Regex

--- welcome.py
+++ welcome.py
@@ -1,7 +1,9 @@
 
 
 print(' Hello there, Enter your name')
-
 name = input()
-
+if not re.match("[a-z]", name): 
+    print(" I Don't need some freakn numbers ")
+    
+else: 
 print('Hello ' + str(name) + ' This is my first program')
